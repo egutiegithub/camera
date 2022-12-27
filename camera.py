0e8fc56a504379e5d31fc38c264e99329b27fc5d
egutie <egutie@gmail.com>
2022-12-26 22:01:15 -0600

    Starting Point

--- camera.py
+++ camera.py
@@ -1,13 +1,22 @@
 # Note: This script runs only on a local IDE with "streamlit run main.py"
 import streamlit as st
 from PIL import Image
- 
+
 st.subheader("Color to Grayscale Converter")
- 
+
+uploaded_image = st.file_uploader("Upload Image")
+
+
+
 with st.expander("Start camera"):
     camera_image = st.camera_input("Camera")
- 
+
 if camera_image:
     img = Image.open(camera_image)
     gray_camera_img = img.convert('L')
     st.image(gray_camera_img)
+
+if uploaded_image:
+    img2 = Image.open(uploaded_image)
+    gray_camera_img2 = img2.convert('L')
+    st.image(gray_camera_img2)
